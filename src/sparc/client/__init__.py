5e8e5d6027a796fcfa6466f94565a3397eef5585
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2022-12-21 11:17:08 +0100

    Update src/sparc/client/__init__.py
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/sparc/client/__init__.py
+++ src/sparc/client/__init__.py
@@ -1,4 +1,4 @@
 import pkg_resources
 from .client import SparcClient
 
-#__version__ = pkg_resources.get_distribution("sparc.client").version
+# __version__ = pkg_resources.get_distribution("sparc.client").version
