3a51e2a9e4a55e8bf4d09978aa30797f04adc8f5
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2023-06-20 16:51:25 +0200

    cleanup

--- src/sparc/client/services/osparc.py
+++ src/sparc/client/services/osparc.py
@@ -1,18 +1,18 @@
-import abc
-import configparser
 import logging
 import os
+from configparser import SectionProxy
+from typing import TypeAlias, Union
 
 import osparc
-
-from ._default import ServiceBase
 from osparc.api import FilesApi, SolversApi
-from osparc.models import File, Job, JobInputs, JobOutputs, JobStatus, Solver, Profile
+from osparc.models import File, Job, JobInputs, JobOutputs, JobStatus, Profile, Solver
 
-# FIXME: adapt to osparc
+from ._default import Any, ServiceBase
+
+ConfigDict: TypeAlias = Union[dict, SectionProxy]
+
 
 class OsparcService(ServiceBase):
-
     default_headers = {
         "Content-Type": "application/json",
         "Accept": "application/json; charset=utf-8",
@@ -20,16 +20,19 @@ class OsparcService(ServiceBase):
 
     host_api = "https://api.osparc.io"
 
-    def __init__(self, config=None, connect=False) -> None:
-        logging.info("Initializing Pennsieve...")
-        logging.debug(str(config))
+    def __init__(self, config: ConfigDict | None = None, connect: bool = False) -> None:
+        config = config or {}
+
+        logging.info("Initializing Osparc...")
+        logging.debug("%s", f"{config}")
 
         self.set_profile(
             osparc_api_key=os.environ.get("OSPARC_API_KEY") or config.get("osparc_api_key"),
-            osparc_api_secret=os.environ.get("OSPARC_API_SECRET") or config.get("osparc_api_secret")
+            osparc_api_secret=os.environ.get("OSPARC_API_SECRET")
+            or config.get("osparc_api_secret"),
         )
 
-        logging.info("Profile: " + self.profile_name)
+        logging.info("Profile: %s", self.profile_name)
         if connect:
             self.connect()  # profile_name=self.profile_name)
 
